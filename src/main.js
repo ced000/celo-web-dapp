01da83dc60caeda4567063ef3ece20acac666a8d
ced000 <egboncedric@gmail.com>
2021-06-16 17:54:38 +0100

    First Dapp on Celo complete (still with bugs)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Web3 from "web3";
 import { newKitFromWeb3 } from "@celo/contractkit";
 import BigNumber from "bignumber.js";
+import marketplaceAbi from "../contract/marketplace.abi.json";
+import erc20Abi from "../contract/erc20.abi.json";
 
 const ERC20_DECIMALS = 18;
+const MPContractAddress = "0x5E7a8aEE5726bAE0750258bce9cba1C33767a672";
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 let kit;
+let contract;
+let products = [];
 
 const connectCeloWallet = async () => {
     if (window.celo) {
@@ -16,8 +22,12 @@ const connectCeloWallet = async () => {
             const web3 = new Web3(window.celo);
             kit = newKitFromWeb3(web3);
 
+            // getting accounts from the kit and setting default account as the first accounts in array
             const accounts = await kit.web3.eth.getAccounts();
             kit.defaultAccount = accounts[0];
+
+            // getting the contract already written and deployed on the blockchain.
+            contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress);
         } catch (error) {
             notification(`⚠️ ${error}.`);
         }
@@ -26,6 +36,12 @@ const connectCeloWallet = async () => {
     }
 }
 
+const approve = async (_price) => {
+    const cUSDContract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress);
+
+    const result = await cUSDContract.methods.approve(MPContractAddress, _price).send({from: kit.defaultAccount});
+    return result;
+}
 // const products = [
 //     {
 //         name: "Giant BBQ",
@@ -79,6 +95,30 @@ const getBalance = async () => {
     document.querySelector("#balance").textContent = cUSDBalance;
 };
 
+const getProducts = async () => {
+    const _productsLength = await contract.methods.getProductsLength().call();
+    const _products = [];
+
+    for(let i = 0; i < _productsLength; i++) {
+        let _product = new Promise(async (resolve, reject) => {
+            let p = await contract.methods.readProduct(i).call();
+            resolve({
+                index: i,
+                owner: p[0],
+                name: p[1],
+                image: p[2],
+                description: p[3],
+                location: p[4],
+                price: new BigNumber(p[5]),
+                sold: p[6]
+            })
+        })
+        _products.push(_product);
+    }
+    products = await Promise.all(_products);
+    renderProducts();
+}
+
 function renderProducts() {
     document.getElementById("marketplace").innerHTML = "";
     products.forEach((_product) => {
@@ -108,7 +148,7 @@ function productTemplate(_product) {
             </p>
             <div class="d-grip gap-2">
                 <a class="btn btn-lg btn-outline-dark buyBtn fs-6 p-3" id=${_product.index}>
-                    Buy for ${_product.price} cUSD
+                    Buy for ${_product.price.shiftedBy(-ERC20_DECIMALS).toFixed(2)} cUSD
                 </a>
             </div>
         </div>
@@ -145,31 +185,52 @@ window.addEventListener("load", async () => {
     notification("⏳ Loading...");
     await connectCeloWallet();
     await getBalance();
+    await getProducts();
     //renderProducts();
     notificationOff();
 })
 
-document.querySelector("#newProductBtn").addEventListener("click", () => {
-    const _product = {
-        owner: "0x2EF48F32eB0AEB90778A2170a0558A941b72BFFb",
-        name: document.getElementById("newProductName").value,
-        image: document.getElementById("newImgUrl").value,
-        description: document.getElementById("newProductDescription").value,
-        location: document.getElementById("newLocation").value,
-        price: document.getElementById("newPrice").value,
-        sold: 0,
-        index: products.length
+document.querySelector("#newProductBtn").addEventListener("click", async (e) => {
+    const params = [
+        document.getElementById("newProductName").value,
+        document.getElementById("newImgUrl").value,
+        document.getElementById("newProductDescription").value,
+        document.getElementById("newLocation").value,
+        new BigNumber(document.getElementById("newPrice").value).shiftedBy(ERC20_DECIMALS).toString()
+    ]
+
+    notification(`🎉 Adding "${params[0]}"...`);
+
+    try {
+        const result = await contract.methods
+        .writeProduct(...params)
+        .send({from: kit.defaultAccount});
+    } catch (error) {
+        notification(`⚠️ ${error}.`)
     }
-    products.push(_product)
-    notification(`🎉 You successfully added "${_product.name}".`)
-    renderProducts()
+    notification(`🎉 You successfully added "${params[0]}"`);
+    getProducts();
 })
 
-document.querySelector("#marketplace").addEventListener("click", (e) => {
+document.querySelector("#marketplace").addEventListener("click", async (e) => {
     if (e.target.className.includes("buyBtn")) {
         const index = e.target.id;
-        products[index].sold++;
-        notification(`🎉 You successfully bought "${products[index].name}".`);
-        renderProducts();
+        notification("⏳ Waiting for payment approval...");
+        try {
+            await approve(products[index].price);
+        } catch (error) {
+            notification(`⚠️ ${error}.`);
+        }
+
+        notification(`⏳ Awaiting payment for "${products[index].name}"...`);
+        try {
+            const result = await contract.methods.buyProduct(index).send({from: kit.defaultAccount});
+            notification(`🎉 You successfully bought "${products[index].name}".`);
+            getProducts();
+            getBalance();
+
+        } catch (error) {
+            notification(`⚠️ ${error}.`);
+        }
     }
 })
